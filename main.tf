e27db50d09d9883fb388495e7ff0725c112c8036
Joe Bell <jbfbell@gmail.com>
2023-11-16 11:07:36 -0800

    add role granting

---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "0.75.0"
+      configuration_aliases = [ snowflake.securityadmin, snowflake.sysadmin]
+    }
+  }
+}
+
+resource "snowflake_role" "airbyte" {
+  provider = snowflake.securityadmin
+  name = var.airbyte_role
+}
+
+resource "snowflake_role_grants" "sysadmin_grants" {
+  provider = snowflake.securityadmin
+  role_name = snowflake_role.airbyte.name
+  roles = ["SYSADMIN"]
+}
+
+resource "snowflake_user" "airbyte_user" {
+  provider = snowflake.securityadmin
+  name = var.airbyte_username
+  password = var.airbyte_password
+  default_role = snowflake_role.airbyte.name
+  default_warehouse = var.airbyte_warehouse_name
+}
+
+resource "snowflake_role_grants" "user_grant" {
+  provider = snowflake.securityadmin
+  role_name = snowflake_role.airbyte.name
+
+  users = [
+    snowflake_user.airbyte_user.name
+  ]
+}
+
+
+resource "snowflake_warehouse_grant" "ab_warehouse_grant" {
+  provider = snowflake.sysadmin
+  warehouse_name = var.airbyte_warehouse_name
+  privilege = "USAGE"
+  roles = [
+    snowflake_role.airbyte.name
+  ]
+}
+
+resource "snowflake_database_grant" "ab_db_grant" {
+  provider = snowflake.sysadmin
+  database_name = var.airbyte_database_name
+  privilege = "OWNERSHIP"
+  roles = [
+    snowflake_role.airbyte.name
+  ]
+}
+
+
+resource "snowflake_schema_grant" "ab_aschema_grant" {
+  for_each = toset(var.airbyte_schema_names)
+  provider = snowflake.sysadmin
+  database_name = var.airbyte_database_name
+  schema_name = each.key
+  privilege = "OWNERSHIP"
+  roles = [
+    snowflake_role.airbyte.name
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "airbyte_role" {
+  type = string
+  default = "AIRBYTE_ROLE"
+}
+
+variable "airbyte_username" {
+  type = string
+  default = "AIRBYTE_USER"
+}
+
+variable "airbyte_warehouse_name" {
+  type = string
+  default = "AIRBYTE_WAREHOUSE"
+}
+
+variable "airbyte_database_name" {
+  type = string
+  default = "AIRBYTE_DATABASE"
+}
+
+variable "airbyte_schema_names" {
+  type = list(string)
+  default = ["AIRBYTE_SCHEMA", "airbyte_internal"]
+}
+
+variable "airbyte_password" {
+  type = string
+  sensitive = true
+}
